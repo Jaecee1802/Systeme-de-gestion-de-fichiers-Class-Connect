1ef493dc8a14987068aac5fde658d866c6bff89f
Jaece1802 <banzuela.johncarlo@epcst.edu.ph>
2025-03-10 15:53:56 +0800

    teacher-log-in system ongoing works atm

---
+++ login/js/fetchData.js
@@ -0,0 +1,50 @@
+//Teacher
+let tchrName = document.getElementById("teacherName");
+let tchrEmail = document.getElementById("teacherEmail");
+let tchrPassword = document.getElementById("teacherPassword");
+let retryPass = document.getElementById("teacherReEnter");
+let signUpButt = document.getElementById("sign-up");
+
+// DROPDOWN
+
+document.addEventListener("DOMContentLoaded", () => {
+    const dropdown = document.getElementById("dropDown");
+    const trigger = document.getElementById("ddown-trigger");
+    const dropdownItems = dropdown.querySelectorAll(".dropdown-item");
+
+    trigger.addEventListener("click", (event) => {
+        event.stopPropagation();
+        dropdown.classList.toggle("is-active");
+    })
+
+    dropdownItems.forEach(item => {
+        item.addEventListener('click', (event) => {
+            event.preventDefault();
+            trigger.querySelector("span").textContent = item.textContent; 
+            dropdown.classList.remove('is-active'); 
+        });
+    });
+
+    document.addEventListener("click", (event) => {
+        if(!dropdown.contains(event.target)){
+            dropdown.classList.remove("is-active");
+        }
+    })
+})
+
+//DROPDOWN
+
+signUpButt.addEventListener('click', () => {
+    const inputName = document.getElementById("teacherName").value;
+    const inputEmail = document.getElementById("teacherEmail").value;
+    const inputPassword = document.getElementById("teacherPassword").value;
+    const inputReEnter = document.getElementById("teacherReEnter").value;
+
+    console.log("Data:", {
+        name: inputName,
+        email: inputEmail,
+        password: inputPassword,
+        reEnter: inputReEnter
+    })
+})
+
