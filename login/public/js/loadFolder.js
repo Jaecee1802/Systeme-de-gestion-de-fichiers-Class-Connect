5163d83042001a14a0bbc08c914b2748871bed5d
Jaece1802 <banzuela.johncarlo@epcst.edu.ph>
2025-03-22 17:29:04 +0800

    fixed

--- login/public/js/loadFolder.js
+++ login/public/js/loadFolder.js
@@ -8,7 +8,17 @@ async function loadFolders(){
     folders.forEach(folder => {
         const folderDiv = document.createElement('div');
         folderDiv.classList.add('subj-folders');
-        folderDiv.innerHTML = "";
+        folderDiv.innerHTML = `<a href="#">
+            <div class="myfile-folders is-flex p-4">
+                <img src="images/folder.png" alt="Folder" class="image is-32x32">
+                <p class="has-text-black mt-2 ml-4">${folder.name}</p> 
+            </div>
+            <div>
+                <button class="button is-danger is-small ml-4 my-2">Delete</button>
+                <button class="button is-warning is-small ml-4 my-2">Rename</button>            
+            </div>
+
+        </a>`;
 
         folderContainer.appendChild(folderDiv);
     });
