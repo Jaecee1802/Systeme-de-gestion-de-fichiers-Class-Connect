93b62aeb25e8bce6f9adb07c23a7906447741337
Jaece1802 <banzuela.johncarlo@epcst.edu.ph>
2025-04-09 11:43:36 +0800

    development of the enrolled subjects has been started

---
+++ login/public/js/modalSubject.js
@@ -0,0 +1,38 @@
+//Create Folder Modal
+const openFolderModal = document.getElementById('folder-button');
+const closeFolderModal = document.getElementById('close-folder-modal');
+const folderModal = document.getElementById('subject-folder-modal');
+
+openFolderModal.addEventListener('click', () => {
+    folderModal.classList.add('is-active');
+});
+
+closeFolderModal.addEventListener('click', () => {
+    folderModal.classList.remove('is-active');
+});
+
+//Delete Folder Modal
+const openDeleteModal = document.getElementById('delete-button');
+const closeDeleteModal = document.getElementById('close-delete-modal');
+const deleteModal = document.getElementById('delete-subject-folder-modal');
+
+openDeleteModal.addEventListener('click', () => {
+    deleteModal.classList.add('is-active');
+});
+
+closeDeleteModal.addEventListener('click', () => {  
+    deleteModal.classList.remove('is-active');
+});
+
+//Rename Folder Modal
+const openRenameModal = document.getElementById('rename-button');
+const closeRenameModal = document.getElementById('close-rename-modal');
+const renameModal = document.getElementById('rename-subject-folder-modal');
+
+openRenameModal.addEventListener('click', () => {
+    renameModal.classList.add('is-active');
+})
+
+closeRenameModal.addEventListener('click', () => {
+    renameModal.classList.remove('is-active');
+})
