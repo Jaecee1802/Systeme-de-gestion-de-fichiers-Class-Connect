92b53146a92966895a64cc252ad67a5c335583bb
Jaece1802 <banzuela.johncarlo@epcst.edu.ph>
2025-05-06 09:32:09 +0800

    NEW DUMP

--- sql/Dump20250506.sql
+++ sql/Dump20250506.sql
@@ -54,9 +54,12 @@ CREATE TABLE `files` (
   `file_path` varchar(255) NOT NULL,
   `folder_name` varchar(255) NOT NULL,
   `upload_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `uploadedBy` int DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `ownerID` int DEFAULT NULL,
+  `ownerRole` enum('teacher','student','admin') NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_file` (`ownerID`),
+  CONSTRAINT `fk_file` FOREIGN KEY (`ownerID`) REFERENCES `teachers` (`teacherID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -65,7 +68,7 @@ CREATE TABLE `files` (
 
 LOCK TABLES `files` WRITE;
 /*!40000 ALTER TABLE `files` DISABLE KEYS */;
-INSERT INTO `files` VALUES (2,'vro','vro.jpeg','uploads/folder/to313m53rrlc1.jpeg','folder','2025-04-21 06:50:14',NULL),(3,'mongo','MongoDB Cheat Sheet.zip','uploads/folder 2/MongoDB Cheat Sheet.zip','folder 2','2025-04-22 06:15:37',NULL);
+INSERT INTO `files` VALUES (1,'application','2.-Application-Form-Programming_fillable_v2025.pdf','uploads/normadoe/2.-Application-Form-Programming_fillable_v2025.pdf','normadoe','2025-05-01 03:29:30',2,'student'),(2,'weeknd','7f336a020c194d8aa2aaf4152c134bdb.jpg','uploads/jin/7f336a020c194d8aa2aaf4152c134bdb.jpg','jin','2025-05-02 07:04:48',1,'teacher');
 /*!40000 ALTER TABLE `files` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -77,13 +80,15 @@ DROP TABLE IF EXISTS `folders`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `folders` (
-  `folderID` int NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `dateofCreation` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `ownerID` int DEFAULT NULL,
-  `ownerType` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`folderID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `ownerRole` enum('teacher','student','admin') NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_user` (`ownerID`),
+  CONSTRAINT `fk_user` FOREIGN KEY (`ownerID`) REFERENCES `teachers` (`teacherID`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -92,8 +97,34 @@ CREATE TABLE `folders` (
 
 LOCK TABLES `folders` WRITE;
 /*!40000 ALTER TABLE `folders` DISABLE KEYS */;
-INSERT INTO `folders` VALUES (2,'folder','2025-04-21 06:50:07',NULL,NULL),(3,'folder 2','2025-04-22 06:15:22',NULL,NULL);
+INSERT INTO `folders` VALUES (7,'normadoe','2025-04-30 07:35:18',2,'student'),(8,'mangdoe','2025-04-30 07:36:54',2,'teacher'),(10,'jin','2025-05-02 07:04:33',1,'teacher');
 /*!40000 ALTER TABLE `folders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `studentgrades`
+--
+
+DROP TABLE IF EXISTS `studentgrades`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `studentgrades` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `activityname` varchar(255) DEFAULT NULL,
+  `grade` tinyint DEFAULT NULL,
+  `overallgrade` tinyint DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `studentgrades`
+--
+
+LOCK TABLES `studentgrades` WRITE;
+/*!40000 ALTER TABLE `studentgrades` DISABLE KEYS */;
+INSERT INTO `studentgrades` VALUES (1,'Activity',50,50);
+/*!40000 ALTER TABLE `studentgrades` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -112,7 +143,7 @@ CREATE TABLE `students` (
   `course` varchar(100) NOT NULL,
   `section` varchar(5) NOT NULL,
   PRIMARY KEY (`studID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -121,7 +152,7 @@ CREATE TABLE `students` (
 
 LOCK TABLES `students` WRITE;
 /*!40000 ALTER TABLE `students` DISABLE KEYS */;
-INSERT INTO `students` VALUES (1,'Marc Doe','marcdoe@email.com','21001','$2b$10$zifE2ORosY12i2zXBjxoFerIL7PFwHur71cFiqFqjX83ti3VCAdN6','Information Technology','IT401'),(2,'Norma Doe','normadoe@email.com','25001','$2b$10$nqh07nBuRai3bxuNnrSok.V/WqmqvsnmduOFv4vgLi.ml1GJRJGQe','Computer Science','CS101');
+INSERT INTO `students` VALUES (2,'Norma Doe','normadoe@gmail.com','25001','$2b$10$XNZs48GNGh7nyvQ3OTpjJevVlaXSwnzeDeQJViwViOD3nn9rH6P5.','Computer Science','CS101'),(3,'John Carlo Banzuela','banzuela.johncarlo@epcst.edu.ph','210039','$2b$10$HiTW9W5/2J7ZwcamJx0xFeLM6d2aU28n4eMkAltjaYy6lUbxFTjPW','Computer Science','CS401'),(4,'John Marky Doe','johnmarkydoe@gmail.com','22020','$2b$10$5PQv4LudVN8E0.L1qHD7yuEMQfzK.3DTvm.6jDgZrYzRFSPfThiXq','Information Technology','IT201');
 /*!40000 ALTER TABLE `students` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -139,9 +170,8 @@ CREATE TABLE `subjectfiles` (
   `file_path` varchar(255) NOT NULL,
   `folder_name` varchar(255) NOT NULL,
   `upload_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `uploadedBy` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -150,7 +180,7 @@ CREATE TABLE `subjectfiles` (
 
 LOCK TABLES `subjectfiles` WRITE;
 /*!40000 ALTER TABLE `subjectfiles` DISABLE KEYS */;
-INSERT INTO `subjectfiles` VALUES (4,'hello vro','to313m53rrlc1.jpeg','subject-uploads/Subject/1745300084267-772939033.jpeg','Subject','2025-04-22 05:34:44',NULL);
+INSERT INTO `subjectfiles` VALUES (5,'react','react-cheat-sheet.pdf','uploads/Kazama\'s Sub/1746429052215-629181156.pdf','Kazama\'s Sub','2025-05-05 07:10:52'),(6,'ai-powered','AI-Powered Course Recommendation System.docx','uploads/Kazama\'s Sub/1746429066757-686755224.docx','Kazama\'s Sub','2025-05-05 07:11:07'),(7,'jjr','JJR Mini Donuts.pptx','uploads/Second Sub/1746486914415-537405400.pptx','Second Sub','2025-05-05 23:15:14');
 /*!40000 ALTER TABLE `subjectfiles` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -175,7 +205,7 @@ CREATE TABLE `subjectfolders` (
 
 LOCK TABLES `subjectfolders` WRITE;
 /*!40000 ALTER TABLE `subjectfolders` DISABLE KEYS */;
-INSERT INTO `subjectfolders` VALUES (4,'Subject','2025-04-22 05:34:19');
+INSERT INTO `subjectfolders` VALUES (3,'Kazama\'s Sub','2025-05-05 06:35:19'),(4,'Second Sub','2025-05-05 23:14:44');
 /*!40000 ALTER TABLE `subjectfolders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -193,7 +223,7 @@ CREATE TABLE `teachers` (
   `teacherPassword` varchar(255) NOT NULL,
   `department` varchar(100) NOT NULL,
   PRIMARY KEY (`teacherID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -202,7 +232,7 @@ CREATE TABLE `teachers` (
 
 LOCK TABLES `teachers` WRITE;
 /*!40000 ALTER TABLE `teachers` DISABLE KEYS */;
-INSERT INTO `teachers` VALUES (1,'Jin Kazama','jinkazama@email.com','$2b$10$h3IsCHU8zHSe6/.NG4ANbOR4/eSXsvyFmxqgLjj3BKLjVIfluOi4m','CS Department');
+INSERT INTO `teachers` VALUES (1,'Jin Kazama','jinkazama@email.com','$2b$10$h3IsCHU8zHSe6/.NG4ANbOR4/eSXsvyFmxqgLjj3BKLjVIfluOi4m','CS Department'),(2,'Mang Doe','mangdoe@gmail.com','$2b$10$fGhMlZGedQVIZoy1YU3UrO0fCHtF9ipgyPZTr.0mz.W9R79pyi0zS','CpE Department');
 /*!40000 ALTER TABLE `teachers` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -215,4 +245,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-04-23 20:17:47
+-- Dump completed on 2025-05-06  9:31:27
